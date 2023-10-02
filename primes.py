10ed9360643eb0b8c793a5da8f30253971d0d44e
Abdullah-Zahran <k22042495@kcl.ac.uk>
2023-10-02 18:56:01 +0100

    Finish Code

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes < 1:
+        raise ValueError()
+    primes = [2]
+    i = 1
+    while len(primes) < number_of_primes:
+        prime = True
+        i+=2
+        for primeNum in primes:
+            if i % primeNum == 0:
+                prime = False
+                break
+        if prime:
+            primes.append(i)
+
+    return primes
